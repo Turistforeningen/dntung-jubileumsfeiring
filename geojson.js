7b5c602280fd729dcd18a541f962a888693e17c1
Hans Kristian Flaatten <hans@starefossen.com>
2013-06-13 14:29:21 +0200

    Adds more activites

--- geojson.js
+++ geojson.js
@@ -42,7 +42,73 @@ var activities = [{
     ,"who": "DNT ung Stavanger"
     ,"url": "http://www2.turistforeningen.no/activity.php?ac_id=49767&fo_id=2513"
   }
+},{
+  "type": "Point"
+  ,"coordinates": [8.77110242843628, 58.458647251735094]
+  ,"properties": {
+    "id": 49587
+    ,"title": "15:00 - ARENDAL"
+    ,"what": "Geocaching"
+    ,"where": "Arendal"
+    ,"who": "DNT ung Aust-Agder"
+    ,"url": "http://www2.turistforeningen.no/activity.php?ac_id=49587&fo_id=2513"
+  }
+},{
+  "type": "Point"
+  ,"coordinates": [7.2368202209473, 59.024398803711]
+  ,"properties": {
+    "id": 45241
+    ,"title": "16:00 - SETESDALSHEIENE"
+    ,"what": "Topptur og fisking"
+    ,"where": "Øyuvsbu (mellom Sirdal og Setesdal)"
+    ,"who": "DNT ung Sør"
+    ,"url": "http://www.dntsor.no/activity.php?ac_id=45241"
+  }
+},{
+  "type": "Point"
+  ,"coordinates": [6.327095031738281, 62.48675477290771]
+  ,"properties": {
+    "id": 46775
+    ,"title": "17:00 - SUNNMØRE"
+    ,"what": "Kajakktur"
+    ,"where": "Ellingsøyfjorden"
+    ,"who": "DNT ung Sunnmøre"
+    ,"url": "http://alesund.turistforeningen.no/activity.php?ac_id=46775"
+  }
+},{
+  "type": "Point"
+  ,"coordinates": [7.2800598144531, 62.183300018311]
+  ,"properties": {
+    "id": 48847
+    ,"title": "18:00 - ROMSDAL"
+    ,"what": "Kveldstur med panoramautsikt mot solnedgangen vest i havet"
+    ,"where": "Torvløysa"
+    ,"who": "DNT ung Romsdal"
+    ,"url": "http://www.mrtur.no/activity.php?ac_id=48847"
+  }
 /*
+},{
+  "type": "Point"
+  ,"coordinates": [5.3633880615234375, 60.409275277271945]
+  ,"properties": {
+    "id": 45028
+    ,"title": "foo"
+    ,"what": "bar"
+    ,"where": "baz"
+    ,"who": "fox"
+    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49709"
+  }
+},{
+  "type": "Point"
+  ,"coordinates": [5.3633880615234375, 60.409275277271945]
+  ,"properties": {
+    "id": 45028
+    ,"title": "foo"
+    ,"what": "bar"
+    ,"where": "baz"
+    ,"who": "fox"
+    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49709"
+  }
 },{
   "type": "Point"
   ,"coordinates": [5.3633880615234375, 60.409275277271945]
