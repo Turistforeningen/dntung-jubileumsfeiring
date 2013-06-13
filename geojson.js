0d13aaa4f5fc7e7e8008765ae1aada3432c0c66d
Hans Kristian Flaatten <hans@starefossen.com>
2013-06-13 13:45:20 +0200

    Updates coordinates

--- geojson.js
+++ geojson.js
@@ -1,20 +1,23 @@
 var activities = [{
   "type": "Point"
-  ,"coordinates": [9, 61]
+  ,"coordinates": [10.733333, 59.911111]
   ,"properties": {
     "id": 1234
     ,"title": "12:00 - OSLO"
     ,"what": "Pangstart! Vi gir dere muligheten til å teste ut en rekke av våre mest populære aktiviteter midt i Oslo sentrum."
     ,"where": "Oslo Sentrum"
     ,"who": "DNT ung Oslo"
+    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49709"
   }
 },{
   "type": "Point"
-  ,"coordinates": [8, 60]
+  ,"coordinates": [5.3633880615234375, 60.409275277271945]
   ,"properties": {
-    "id": 1234
-    ,"title": "Navn på aktivitet"
-    ,"desc": "Beskrivelse på aktivitet"
-    ,"url": "http://dntung.no/"
+    "id": 45028
+    ,"title": "Vidden by night"
+    ,"what": "Beskrivelse på aktivitet"
+    ,"where": "Vidden"
+    ,"who": "DNT ung Bergen"
+    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49709"
   }
 }]
