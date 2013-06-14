5ffbf3df905a66d08087a7f4d1ad22ac5991c117
Hans Kristian Flaatten <hans@starefossen.com>
2013-06-14 14:17:45 +0200

    Adds all the missing activities

--- geojson.js
+++ geojson.js
@@ -1,6 +1,6 @@
 var activities = [{
   "type": "Point"
-  ,"coordinates": [10.733333, 59.911111]
+  ,"coordinates": [10.736230909824371, 59.914036394375366]
   ,"properties": {
     "id": 1234
     ,"title": "12:00 - OSLO"
@@ -22,25 +22,18 @@ var activities = [{
   }
 },{
   "type": "Point"
-  ,"coordinates": [6.327095031738281, 62.48675477290771]
-  ,"properties": [{
+  ,"coordinates": [6.099643707275391, 62.459463239996076]
+  ,"properties": {
     "id": 49564
     ,"title": "13:00 - ÅLESUND"
     ,"what": "Lunsjfjelltur til Sukkertoppen i Ålesund"
     ,"where": "Sukkertoppen"
     ,"who": "DNT ung Sunnmøre"
     ,"url": "http://www2.turistforeningen.no/activity.php?ac_id=49564&fo_id=2513"
-  },{
-    "id": 46775
-    ,"title": "17:00 - SUNNMØRE"
-    ,"what": "Kajakktur"
-    ,"where": "Ellingsøyfjorden"
-    ,"who": "DNT ung Sunnmøre"
-    ,"url": "http://alesund.turistforeningen.no/activity.php?ac_id=46775"
-  }]
+  }
 },{
   "type": "Point"
-  ,"coordinates": [6, 59]
+  ,"coordinates": [5.7291340827941895, 58.97155004699195]
   ,"properties": {
     "id": 49767
     ,"title": "14:00 - STAVANGER"
@@ -62,7 +55,7 @@ var activities = [{
   }
 },{
   "type": "Point"
-  ,"coordinates": [7.2368202209473, 59.024398803711]
+  ,"coordinates": [7.236728668212891, 59.02454506942237]
   ,"properties": {
     "id": 45241
     ,"title": "16:00 - SETESDALSHEIENE"
@@ -73,7 +66,18 @@ var activities = [{
   }
 },{
   "type": "Point"
-  ,"coordinates": [7.2800598144531, 62.183300018311]
+  ,"coordinates": [6.327095031738281, 62.48675477290771]
+  ,"properties": {
+    "id": 46775
+    ,"title": "17:00 - SUNNMØRE"
+    ,"what": "Kajakktur"
+    ,"where": "Ellingsøyfjorden"
+    ,"who": "DNT ung Sunnmøre"
+    ,"url": "http://alesund.turistforeningen.no/activity.php?ac_id=46775"
+  }
+},{
+  "type": "Point"
+  ,"coordinates": [7.5839996337890625, 62.635663355954776]
   ,"properties": {
     "id": 48847
     ,"title": "18:00 - ROMSDAL"
@@ -82,61 +86,213 @@ var activities = [{
     ,"who": "DNT ung Romsdal"
     ,"url": "http://www.mrtur.no/activity.php?ac_id=48847"
   }
-/*
 },{
   "type": "Point"
-  ,"coordinates": [5.3633880615234375, 60.409275277271945]
+  ,"coordinates": [10.25721788406372, 59.084669226863575]
   ,"properties": {
-    "id": 45028
-    ,"title": "foo"
-    ,"what": "bar"
-    ,"where": "baz"
-    ,"who": "fox"
-    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49709"
+    "id": 49669
+    ,"title": "18:00 - VESTFOLD"
+    ,"what": "Våttkortkurs/overnattingstur"
+    ,"where": "Buerøya"
+    ,"who": "DNT ung Sandefjord"
+    ,"url": "http://www2.turistforeningen.no/activity.php?ac_id=49669&fo_id=2513"
   }
 },{
   "type": "Point"
-  ,"coordinates": [5.3633880615234375, 60.409275277271945]
+  ,"coordinates": [10.832390785217285, 63.53970006636008]
+  ,"properties": [{
+    "id": 49764
+    ,"title": "18.00 - TRØNDELAG"
+    ,"what": "Aktivitetsdøgn med masse aktiviteter"
+    ,"where": "Håmmårsbukta"
+    ,"who": "DNT ung Trøndelag"
+    ,"url": "http://www.dntung.no/trondelag/activity.php?ac_id=49764"
+  },{
+    "id": 49764
+    ,"title": "11:00 - TRØNDELAG"
+    ,"what": "Salutt, padling og andre aktiviteter"
+    ,"where": "Håmmårsbukta"
+    ,"who": "DNT ung Trøndelag"
+    ,"url": "http://www.dntung.no/trondelag/activity.php?ac_id=49764"
+  }]
+},{
+  "type": "Point"
+  ,"coordinates": [10.718536376953125, 59.87177762221592]
   ,"properties": {
-    "id": 45028
-    ,"title": "foo"
-    ,"what": "bar"
-    ,"where": "baz"
-    ,"who": "fox"
-    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49709"
+    "id": 49648
+    ,"title": "19:00 - OSLO"
+    ,"what": "Sommeraktiviteter og moro"
+    ,"where": "Hovedøya"
+    ,"who": "DNT ung Oslo"
+    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49648"
   }
 },{
   "type": "Point"
-  ,"coordinates": [5.3633880615234375, 60.409275277271945]
-  ,"properties": {
-    "id": 45028
-    ,"title": "foo"
-    ,"what": "bar"
-    ,"where": "baz"
-    ,"who": "fox"
-    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49709"
-  }
+  ,"coordinates": [14.224205017089844, 67.1640264478416]
+  ,"properties": [{
+    "id": 49006
+    ,"title": "20:00 - BODØ"
+    ,"what": "Volleyballturnering og andre ballaktiviteter!"
+    ,"where": "Bodø"
+    ,"who": "DNT ung Bodø"
+    ,"url": "http://www2.turistforeningen.no/activity.php?ac_id=49006&fo_id=2513"
+  },{
+    "id": 49006
+    ,"title": "24:00 - BODØ"
+    ,"what": "Topptur"
+    ,"where": "Sandhornet"
+    ,"who": "DNT ung Bodø"
+    ,"url": "http://www.dntung.no/bodo/activity.php?ac_id=49006"
+  },{
+     "id": 49006
+     ,"title": "06:00 - BODØ"
+     ,"what": "Nattbad og bålkos"
+     ,"where": "Sandvikstranda"
+     ,"who": "DNT ung Bodø"
+    ,"url": "http://www.dntung.no/bodo/activity.php?ac_id=49006"
+  },{
+     "id": 49006
+     ,"title": "07:00 - BODØ"
+     ,"what": "Slakkline, frokost og kake"
+     ,"where": "Sandvikstranda"
+     ,"who": "DNT ung Bodø"
+    ,"url": "http://www.dntung.no/bodo/activity.php?ac_id=49006"
+  }]
+},{
+  "type": "Point"
+  ,"coordinates": [11.127476692199707, 59.67204236341757]
+  ,"properties": [{
+    "id": 48941
+    ,"title": "21:00 - SPYDEBERG"
+    ,"what": "Lær å padle kano"
+    ,"where": "Lyseren"
+    ,"who": "DNT ung Indre Østfold"
+    ,"url": "http://indreostfold.turistforeningen.no/activity.php?ac_id=48941"
+  },{
+    "id": 48941
+    ,"title": "22:00 - SPYDEBERG"
+    ,"what": "Kanotur i solnedgang"
+    ,"where": "Lyseren"
+    ,"who": "DNT ung Indre Østfold"
+    ,"url": "http://indreostfold.turistforeningen.no/activity.php?ac_id=48941"
+  },{
+     "id": 48941
+     ,"title": "02:00 - SPYDEBERG"
+     ,"what": "Nattbad"
+     ,"where": "Lyseren"
+     ,"who": "DNT ung Indre Østfold"
+    ,"url": "http://indreostfold.turistforeningen.no/activity.php?ac_id=48941"
+  },{
+     "id": 49006
+     ,"title": "08:00 - SPYDEBERG"
+     ,"what": "Fiskekonkurranse med premie til største fisk"
+     ,"where": "Lyseren"
+     ,"who": "DNT ung Indre Østfold"
+    ,"url": "http://indreostfold.turistforeningen.no/activity.php?ac_id=48941"
+  }]
 },{
   "type": "Point"
   ,"coordinates": [5.3633880615234375, 60.409275277271945]
-  ,"properties": {
+  ,"properties": [{
     "id": 45028
-    ,"title": "foo"
-    ,"what": "bar"
-    ,"where": "baz"
-    ,"who": "fox"
-    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49709"
+    ,"title": "22:00 - BERGEN"
+    ,"what": "Vidden by night"
+    ,"where": "Vidden i Bergen"
+    ,"who": "DNT ung Bergen"
+    ,"url": "http://www.dntung.no/bergen/activity.php?ac_id=45028"
+},{
+    "id": 45028
+    ,"title": "01:00 - BERGEN"
+    ,"what": "Grilling på Vidden"
+    ,"where": "Vidden i Bergen"
+    ,"who": "DNT ung Bergen"
+    ,"url": "http://www.dntung.no/bergen/activity.php?ac_id=45028"
+},{
+    "id": 45028
+    ,"title": "06:00 - BERGEN"
+    ,"what": "Frokost ved Ulrikken"
+    ,"where": "Bergen"
+    ,"who": "DNT ung Bergen"
+    ,"url": "http://www.dntung.no/bergen/activity.php?ac_id=45028"
+  }]
+},{
+  "type": "Point"
+  ,"coordinates": [18.321762084960938, 69.73262083702681]
+  ,"properties": [{
+    "id": 48981
+    ,"title": "23:00 - TROMSØ"
+    ,"what": "Midnattsoltur"
+    ,"where": "Skamtinden"
+    ,"who": "DNT ung Troms"
+    ,"url": "http://troms.turistforeningen.no/activity.php?ac_id=48981"
+},{
+    "id": 48981
+    ,"title": "04:00 - TROMSØ"
+    ,"what": "Overnatting under midnattsol, med storhavet som nærmeste nabo"
+    ,"where": "Tromsø"
+    ,"who": "DNT ung Troms"
+    ,"url": "http://troms.turistforeningen.no/activity.php?ac_id=48981"
+},{
+    "id": 48981
+    ,"title": "12:00 - TROMSØ"
+    ,"what": "Kajakkpadling"
+    ,"where": "Grøtfjord"
+    ,"who": "DNT ung Troms"
+    ,"url": "http://troms.turistforeningen.no/activity.php?ac_id=48981"
+  }]
+},{
+  "type": "Point"
+  ,"coordinates": [8.649144, 59.854428]
+  ,"properties": {
+    "id": 49552
+    ,"title": "03:00 - GAUSTATOPPEN"
+    ,"what": "Soloppgangtur"
+    ,"where": "Gaustatoppen"
+    ,"who": "DNT ung Telemark"
+    ,"url": "http://www.dntung.no/telemark/activity.php?ac_id=49552"
   }
 },{
   "type": "Point"
-  ,"coordinates": [5.3633880615234375, 60.409275277271945]
+  ,"coordinates": [5.4296875, 59.28796455820375]
   ,"properties": {
-    "id": 45028
-    ,"title": "foo"
-    ,"what": "bar"
-    ,"where": "baz"
-    ,"who": "fox"
-    ,"url": "http://www.dntung.no/oslo/activity.php?ac_id=49709"
+    "id": 45853
+    ,"title": "09:00 - HAUGESUND"
+    ,"what": "KystBaseCamp"
+    ,"where": "Vesterøy"
+    ,"who": "DNT ung Haugesund"
+    ,"url": "http://www.dntung.no/haugesund/activity.php?ac_id=45853"
   }
-*/
+},{
+  "type": "Point"
+  ,"coordinates": [7.3175811767578125, 61.385540185126295]
+  ,"properties": {
+    "id": 46858
+    ,"title": "10:00 - LUSTER"
+    ,"what": "Kajakkurs"
+    ,"where": "Luster"
+    ,"who": "DNT ung Luster"
+    ,"url": "http://luster.turistforeningen.no/activity.php?ac_id=46858"
+  }
+},{
+  "type": "Point"
+  ,"coordinates": [11.056537628173828, 61.10063120735526]
+  ,"properties": {
+    "id": 49570
+    ,"title": "12:00 - HAMAR"
+    ,"what": "Villmarksmoro med uhøytidelige konkurranser"
+    ,"where": "Bringbu"
+    ,"who": "DNT ung Hamar og Hedemarken"
+    ,"url": "http://www2.turistforeningen.no/activity.php?ac_id=49570&fo_id=2513"
+  }
+},{
+  "type": "Point"
+  ,"coordinates": [8.00205945968628, 58.133678049349825]
+  ,"properties": {
+    "id": 45245
+    ,"title": "13:00- KRISTIANSAND"
+    ,"what": "Klatre og slakkline-dag"
+    ,"where": "Odderøya"
+    ,"who": "DNT ung Sør"
+    ,"url": "http://www.dntsor.no/activity.php?ac_id=45245"
+  }
 }]
